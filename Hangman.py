2d8270463700960f0cefdee20e9c80847a931adb
Howard Cennatha <howardcenn@gmail.com>
2025-11-28 19:54:00 +0700

    Main Function

---
+++ Hangman.py
@@ -0,0 +1,49 @@
+import random
+
+words = ('apple', 'orange', 'banana', 'coconut', 'pineapple')
+
+hangman_art = {0: ("   ",
+                   "   ",
+                   "   "),
+               1: (" o ",
+                   "   ",
+                   "   "),
+               2: (" o ",
+                   " | ",
+                   "   "),
+               3: (" o ",
+                   "/| ",
+                   "   "),
+               4: (" o ",
+                   "/|\\",
+                   "   "),
+               5: (" o ",
+                   "/|\\",
+                   "/  "),
+               6: (" o ",
+                   "/|\\",
+                   "/ \\")}
+
+def display_man(wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+if __name__ == "__main__":
+    main()

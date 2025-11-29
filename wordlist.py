7090fc887013e89f18bfb5558409218ca23db563
Howard Cennatha <howardcenn@gmail.com>
2025-11-29 16:34:34 +0700

    ACSCII Art FInal

---
+++ wordlist.py
@@ -0,0 +1,24 @@
+# Kata untuk Hangman Game
+
+words = (
+    'cat', 'dog', 'fish', 'bird', 'cow',
+    'chicken', 'duck', 'goat', 'sheep', 'horse',
+    'rabbit', 'frog', 'snake', 'turtle', 'lion',
+    'tiger', 'elephant', 'monkey', 'bear', 'fox',
+    'wolf', 'pig', 'mouse', 'rat', 'deer',
+    'giraffe', 'zebra', 'panda', 'koala', 'kangaroo',
+    'camel', 'donkey', 'buffalo', 'ox', 'turkey',
+    'eagle', 'owl', 'parrot', 'penguin', 'flamingo',
+    'dove', 'bat', 'swan', 'peacock', 'crow',
+    'shark', 'whale', 'dolphin', 'octopus', 'crab',
+    'lobster', 'jellyfish', 'seal', 'sea lion', 'starfish',
+    'ant', 'bee', 'butterfly', 'spider', 'fly',
+    'mosquito', 'grasshopper', 'ladybug', 'cockroach', 'worm',
+    'catfish', 'goldfish', 'salmon', 'shrimp', 'seahorse',
+    'hippo', 'rhino', 'crocodile', 'alligator', 'chimpanzee',
+    'gorilla', 'hamster', 'hedgehog', 'squirrel', 'otter',
+    'fox', 'raccoon', 'skunk', 'moose', 'reindeer',
+    'hawk', 'falcon', 'vulture', 'stork', 'woodpecker',
+    'bee', 'wasp', 'dragonfly', 'moth', 'termite',
+    'boar', 'lynx', 'leopard', 'cheetah', 'hyena'
+)
